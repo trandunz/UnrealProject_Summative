c22df7dfef880ae1dcf074c226ddb5c70c264e6b
trandunz <54924605+trandunz@users.noreply.github.com>
2022-06-01 22:43:35 +1200

    + Moving Platform(basic)

---
+++ GDP03_Summative2/Source/GDP03_Summative2/MovingPlatform.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Platform");
+	RootComponent = Mesh;
+
+	m_MoveDirection = 1;
+	
+
+	bReplicates = true;
+	SetReplicateMovement(true);
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	m_StartLocation = GetActorLocation();
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector location = GetActorLocation();
+
+	if (location.X < m_StartLocation.X - 200 || location.X > m_StartLocation.X + 200)
+		m_MoveDirection *= -1;
+
+	location.X += m_MoveDirection * MoveSpeed;
+
+	SetActorLocation(location);
+}
+
---
+++ GDP03_Summative2/Source/GDP03_Summative2/MovingPlatform.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingPlatform.generated.h"
+
+UCLASS()
+class GDP03_SUMMATIVE2_API AMovingPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMovingPlatform();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+		UStaticMeshComponent* Mesh;
+	UPROPERTY(EditAnywhere)
+		float MoveSpeed;
+
+private:
+	FVector m_StartLocation;
+	int m_MoveDirection;
+};
